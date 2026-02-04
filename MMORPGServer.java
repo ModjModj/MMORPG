065f7278d473eba218c3f77a287b442c52219d58
ModjModj <156004929+ModjModj@users.noreply.github.com>
2026-02-04 09:03:41 -0800

    Fix loop variable declaration in MMORPGServer

--- MMORPGServer.java
+++ MMORPGServer.java
@@ -61,7 +61,7 @@ class PlayerHandler implements Runnable {
 
             out.println("Enter your player name:");
             playerName = in.readLine();
-            for (i = 0; i <= 100000000; i++){
+            for (int i = 0; i <= 100000000; i++){
             MMORPGServer.addPlayer(playerName, this);
             System.out.println(playerName + " has joined the game.");
             }
